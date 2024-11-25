e289d0b9de1c0541807aee8353a8339afd05ba0a
Yelzhas1 <elzhas_1998@mail.ru>
2024-11-25 17:51:03 +0500

    Добавьте файлы проекта.

---
+++ Modul13/Task1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        // Путь к текстовому файлу
+        string filePath = "C:\\Users\\Diana\\Documents\\Text1.txt";
+
+        try
+        {
+            string text = File.ReadAllText(filePath);
+
+            int[] numbers = Array.ConvertAll(text.ToCharArray(), c => (int)c);
+
+            const int itemCount = 100000; 
+
+            // 1. Тестируем List<T>
+            List<int> list = new List<int>();
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            foreach (var num in numbers)
+            {
+                list.Insert(list.Count / 2, num);
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"List<T>: Вставка {numbers.Length} элементов заняла {stopwatch.ElapsedMilliseconds} ms");
+
+            // 2. Тестируем LinkedList<T>
+            LinkedList<int> linkedList = new LinkedList<int>();
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            var node = linkedList.First;
+            foreach (var num in numbers)
+            {
+                if (node == null)
+                {
+                    linkedList.AddFirst(num);
+                }
+                else
+                {
+                    linkedList.AddAfter(node, num);
+                    node = node.Next;
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"LinkedList<T>: Вставка {numbers.Length} элементов заняла {stopwatch.ElapsedMilliseconds} ms");
+
+            Console.WriteLine("Тест завершен.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+}
+
+//List<T>: Вставка 933318 элементов заняла 25010 ms
+//LinkedList<T>: Вставка 933318 элементов заняла 120 ms
+//Тест завершен.
---
+++ Modul13_2/Task2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Путь к текстовому файлу
+        string filePath = "C:\\Users\\Diana\\Documents\\Text2.txt";
+
+        try
+        {
+            string text = File.ReadAllText(filePath);
+
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            string[] words = noPunctuationText.Split(new[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+            words = words.Select(w => w.ToLower()).ToArray();
+
+            var wordCounts = words.GroupBy(word => word)
+                                  .Select(group => new { Word = group.Key, Count = group.Count() })
+                                  .OrderByDescending(word => word.Count)
+                                  .Take(10);            
+
+            Console.WriteLine("Топ 10 наиболее часто встречающихся слов:");
+            foreach (var word in wordCounts)
+            {
+                Console.WriteLine($"Слово: {word.Word}, Количество: {word.Count}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+}
+
+//Топ 10 наиболее часто встречающихся слов:
+//Слово: и, Количество: 6026
+//Слово: не, Количество: 3902
+//Слово: в, Количество: 3525
+//Слово: он, Количество: 2625
+//Слово: что, Количество: 2412
+//Слово: на, Количество: 2401
+//Слово: с, Количество: 2036
+//Слово: она, Количество: 1634
+//Слово: а, Количество: 1574
+//Слово: как, Количество: 1536
